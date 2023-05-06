0ee5cc4b9548ee1fd3c6c462fbac0fe7160cf3d9
Denis <denisring@protonmail.com>
2023-05-06 14:05:51 +0100

    Fixes

--- webapp.py
+++ webapp.py
@@ -2,8 +2,10 @@ import streamlit as st
 import plotly.express as px
 import pandas
 
+
 df = pandas.read_csv("data.txt")
 
-figure = px.line(x=df["date"],y=df["temperature"],  labels={"x": "Date", "y": "Temperature (C)"})
+figure = px.line(x=df["date"], y=df["temperature"],
+                             labels={"x": "Date", "y": "Temperature (C)"})
 
-st.ploly_chart(figure)
+st.plotly_chart(figure)
